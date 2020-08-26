3b67b66eba087c658fcdf7997f4d411ef7428838
Jason Jones <jasonroyjones@gmail.com>
2020-08-26 14:18:50 -0700

    basic setup of boilerplate started

---
+++ client/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+// import the search component
+// import Characters from './components/Characters';
+
+import "./stylesheets/styles.css";
+
+const App = props => {
+  return (
+    <div className="router">
+      <main>
+        {/*
+            NOTE: The syntax below is for React-Router
+              - A helpful library for routing with a React app.
+              You can learn more about this at:
+              https://reacttraining.com/react-router/web/guides/quick-start
+        */}
+        <Switch>
+          <Route exact path="/" component={Characters} />
+          <Route exact path="/create" component={CreateCharacter} />
+        </Switch>
+      </main>
+    </div>
+  );
+};
+
+export default App;
